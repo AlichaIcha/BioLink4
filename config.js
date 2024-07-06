f39777f51ccff5a8535582205dad49f40bf40243
AlichaXD <dheriap248@gmail.com>
2024-07-06 16:28:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  profilePicture: "https://telegra.ph/file/8a0429de145cf8b06c0d8.jpg",
+  popupPicture: "https://telegra.ph/file/8a0429de145cf8b06c0d8.jpg",
+  popupQuote: "But... the sensation that I've lost something lingers for a longtime after I wake up.",
+  userName: "@AlichaDinastira",
+  links: [
+    { href: "https://github.com/AlichaIcha", icon: "fab fa-github", label: "Github" },
+    { href: "https://wa.me/6281400346604", icon: "fab fa-whatsapp", label: "WhatsApp" },
+    { href: "https://t.me/FadlanDev", icon: "fab fa-facebook", label: "Telegram" },
+    { href: "https://www.instagram.com/fadlangenz", icon: "fab fa-instagram", label: "Instagram" },
+    { href: "https://twitter.com/ApDheri94512", icon: "fab fa-twitter", label: "Twitter" },
+    { href: "https://youtu.be/9DD4PDTQKMw?si=6dxVb-EaG_1EIhNi", icon: "fa fa-play-circle", label: "Menyala Dj Ku" }
+  ]
+};
